d00f510ab9a2507c4c1e1aeabcc94575fd4ca112
chiouwur <chioisidorav@gmail.com>
2024-05-09 20:04:32 -0400

    comentario

--- active/bd.php
+++ active/bd.php
@@ -11,4 +11,10 @@ try{
     echo $ex->getMessage();
 }
 
+#El código que has proporcionado en PHP es una conexión a una base de datos, utilizando el PDO (PHP Data Object) de MySQL.
+#Intenta conectarse a una base de datos llamada «activos» en «localhost» (que es el nombre del servidor que está ejecutando el código PHP) con el usuario «root» y sin contraseña.
+#Si la conexión tiene éxito, el objeto PDO `$conexion` será creado y puede ser usado para ejecutar consultas contra la base de datos.
+#Si la conexión falla, el objeto de excepción `$ex` será creado y el mensaje de error será impreso en la salida estándar.
+
 ?>
+
